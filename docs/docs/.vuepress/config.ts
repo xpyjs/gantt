8dfe0c2522ddcb8aaa24f4d296a0b6a28f51663c
jeremyjone <jeremyjone@qq.com>
2021-11-15 12:16:42 +0800

    chore(docs): update docs by action

--- docs/docs/.vuepress/config.ts
+++ docs/docs/.vuepress/config.ts
@@ -6,7 +6,7 @@ export default defineUserConfig<DefaultThemeOptions>({
   lang: 'zh-CN',
   title: 'Jz Gantt Docuemnt',
   description: 'Jz-Gantt document site',
-  base: '/gantt/',
+  base: '/jz-gantt/',
 
   head: [
     [
