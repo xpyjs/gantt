e3f5f3caf880a26c60b7f8c62dff03c7e2773472
jeremyjone <jeremyjone@qq.com>
2021-11-15 10:34:53 +0800

    chore(other): change eslint about console

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,8 +43,11 @@ module.exports = {
     }
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console':
+      process.env.NODE_ENV === 'production'
+        ? ['error', { allow: ['warn', 'error'] }]
+        : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 
     // 解决 prettier 行尾报错
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
